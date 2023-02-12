7d845012ed100d8e0506c6cf5e0537a725e9fb69
cristobal <92489888+Crisppu@users.noreply.github.com>
2023-02-12 17:20:20 -0600

    primera subida del proyecto conversor de moneda

---
+++ src/conversormonedas/Moneda.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conversormonedas;
+
+/**
+ *
+ * @author Crist
+ */
+public class Moneda extends Cambio{
+    
+    public Moneda(String nombre, double valorReferente) {
+        super(nombre, valorReferente);
+    }
+    
+}
+
